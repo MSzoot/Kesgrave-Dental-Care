c767134425802d6b8cbee98922852c8b9671a1e2
Mszoot <marcinszoot@gmail.com>
2025-02-09 15:05:04 +0000

    date js update

---
+++ variables.js
@@ -0,0 +1,9 @@
+const lastUpdateDate = "02.2025"; // Update this date when needed
+
+// Function to insert the date into the page
+function displayLastUpdate(selector) {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach(el => {
+        el.textContent = lastUpdateDate;
+    });
+}
