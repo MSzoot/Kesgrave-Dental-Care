022ebad2cbc97ffa3e55d9f973f5ab5fa2bd4f02
Mszoot <marcinszoot@gmail.com>
2025-05-07 17:08:56 +0100

    Pricing update

--- variables.js
+++ variables.js
@@ -1,4 +1,4 @@
-const lastUpdateDate = "02.2025"; // Update this date when needed
+const lastUpdateDate = "05.2025"; // Update this date when needed
 
 // Function to insert the date into the page
 function displayLastUpdate(selector) {
